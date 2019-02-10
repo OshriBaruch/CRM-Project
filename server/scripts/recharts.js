c5e841c10884ebf38fe49bb0b3ce599506b07b6b
אושרי ברוך <osher.baruch1@gmail.com>
2019-02-10 14:16:44 +0200

    uploading fixed data to heroku

--- server/scripts/recharts.js
+++ server/scripts/recharts.js
@@ -46,36 +46,33 @@ class recharts {
     }
     generateSalesBy(c) {
         c.sold ? this.salesBy.country[c.country] ? this.salesBy.country[c.country]++ : this.salesBy.country[c.country] = 1 : null
+
         c.sold ? this.salesBy.owner[c.owner] ? this.salesBy.owner[c.owner]++ : this.salesBy.owner[c.owner] = 1 : null
 
         c.emailType ? this.salesBy.email[c.emailType] ? this.salesBy.email[c.emailType]++ : this.salesBy.email[c.emailType] = 1 : null
-        this.salesBy.month[moment(c.firstContact).format("MMM")] ? this.salesBy.month[moment(c.firstContact).format("MMM")]++ : this.salesBy.month[moment(c.firstContact).format("MMM")] = 1
+
+        this.salesBy.month[moment(c.firstContact).format("MMM")] ?
+            this.salesBy.month[moment(c.firstContact).format("MMM")]++ : this.salesBy.month[moment(c.firstContact).format("MMM")] = 1
     }
     generateSalesSince(c) {
         moment(c.firstContact).format("MMM YY") === moment("20180609T08").format("MMM YY") ? this.salesSince[moment(c.firstContact).format("MMM-D")] ?
             this.salesSince[moment(c.firstContact).format("MMM-D")]++ : this.salesSince[moment(c.firstContact).format("MMM-D")] = 1 : null
     }
 
-    // naming for conditions - instead of long ifs, create functions that represent that if
     generateClientAcquisitions(c) {
+        const iמTheLastMonth = () => moment(c.firstContact).format("MMM YY") === moment().format("MMM YY")
+        const inSixMonthsAndSameYear = () => moment(c.firstContact).format("l").split("/")[2] === moment().subtract(6, 'month').format("l").split("/")[2] &&
+            moment(c.firstContact).format("l").split("/")[0] > moment().subtract(6, 'month').format("l").split("/")[0]
+        const inSixMonthsAndNotSameYear = () => moment(c.firstContact).format("l").split("/")[2] > moment().subtract(6, 'month').format("l").split("/")[2] &&
+            moment(c.firstContact).format("l").split("/")[0] < moment().subtract(6, 'month').format("l").split("/")[0]
+        const moreThenSixMonths = () => moment(c.firstContact).format("l").split("/")[2] === moment().subtract(6, 'month').format("l").split("/")[2]
 
-        const isLastMonth = () => moment(c.firstContact).format("MMM YY") === moment().format("MMM YY")
-
-        if (isLastMonth()) {
-            this.clientAcquisitions.corrntMonths++;
-        }
-        else if (moment(c.firstContact).format("l").split("/")[2] === moment().subtract(6, 'month').format("l").split("/")[2] &&
-            moment(c.firstContact).format("l").split("/")[0] > moment().subtract(6, 'month').format("l").split("/")[0]) {
-            this.clientAcquisitions.lastSixMonths++
-        }
-        else if (moment(c.firstContact).format("l").split("/")[2] > moment().subtract(6, 'month').format("l").split("/")[2] &&
-            moment(c.firstContact).format("l").split("/")[0] < moment().subtract(6, 'month').format("l").split("/")[0]) {
-            this.clientAcquisitions.lastSixMonths++
-        }
-        else if (moment(c.firstContact).format("l").split("/")[2] === moment().subtract(6, 'month').format("l").split("/")[2]) {
-            this.clientAcquisitions.moreThanSixMonths++
-        }
+        if (iמTheLastMonth()) this.clientAcquisitions.corrntMonths++;
+        else if (inSixMonthsAndSameYear()) this.clientAcquisitions.lastSixMonths++
+        else if (inSixMonthsAndNotSameYear()) this.clientAcquisitions.lastSixMonths++
+        else if (moreThenSixMonths()) this.clientAcquisitions.moreThanSixMonths++
     }
+
     getRecharts() {
         this.clients.map(client => {
             this.generateTopEmployees(client)
